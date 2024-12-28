06a54ddc2817b360431077cf6a45a39d48caf1a5
SahilGawade <sahilgawade828@gmail.com>
2024-12-28 11:56:49 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,71 @@
+// single line comment
+
+/**
+ * this is
+ * a
+ * multi line
+ * comment
+ */
+
+// commented block of code:
+// console.log("hello world");
+// process.stdout.write("hello World");
+// process.stdout.write("hello World");
+// console.table({ name: "admin" });
+// console.table({ fruits: "pineapple" });
+// console.warn({ name: "admin" });
+
+/**
+ * data types in JS:
+ * String (collection of characters)
+ * Number (both int and float)
+ * Bigint (used to store 2^53-1 length integers)
+ * Boolean (true or false)
+ * undefined (data which cannot be defined)
+ * null (empty)
+ * object (collection or similar to array)
+ * symbol (unique value or unique data)
+ */
+
+//variables
+// number
+var marks = 15;
+console.log("marks=", marks);
+
+// old method : allocates memory  to data in ram
+//var:
+//Function-scoped. Variables declared with var are accessible throughout the entire function they are declared in, regardless of block scope (code within curly braces).
+
+let marks1 = 25;
+console.log("marks1=", marks1);
+
+//new method to create variables
+//let:
+//Block-scoped. Variables declared with let are only accessible within the block they are defined in. This includes if statements, loops, and other code blocks.
+
+let temperature = 28.0;
+console.log("temperature =", temperature);
+
+//string-----------------
+let name = "ajay";
+console.log("name=", name);
+
+// boolean------------------------
+let islogin = true;
+console.log("islogin", islogin);
+
+// Bigint---------------
+let biggernum = 4.5035996e15;
+console.log("biggernum = ", biggernum);
+
+//object
+let fruits = ["apples", "cherry", "banana", "pineapple", "mango"];
+console.table(fruits);
+
+let emp = {
+  name: "ajay",
+  hotel: "thanda mamala",
+  position: "waiter ",
+  sal: 2,
+};
+console.table(emp);
